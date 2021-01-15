fab71ca9bf8b736c90620a05884cc0cfa7cd5d9f
Wonsei12 <77491463+Wonsei12@users.noreply.github.com>
2021-01-15 21:50:42 +0900

    Create ps.cpp

---
+++ 582/ps.cpp
@@ -0,0 +1,35 @@
+#define LOCAL
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx,avx2")
+
+#define IOS ios::sync_with_stdio(false);cin.tie(0)
+#define all(x) x.begin(), x.end()
+#define ff first
+#define ss second
+#define LLINF 0x3f3f3f3f3f3f3f3f
+#define INF 0x3f3f3f3f
+#define uniq(x) sort(all(x)); x.resize(unique(all(x))-x.begin());
+#define sz(x) (int)x.size()
+#define pw(x) (1LL<<x)
+
+using pii = pair<int, int>;
+using ll = long long;
+const ll MOD = 1e9 + 7;
+const long double PI = acos(-1.0);
+
+void solve() {
+	
+}
+
+int main() {
+	IOS;
+	int t = 1;
+	while(t--)
+		solve();
+}
